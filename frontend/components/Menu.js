a3b58c28bbe0055de5badbd13503b42675dd0950
Mike Reinhard <mike.reinhard@fractureme.com>
2019-02-08 15:25:04 -0500

    dynamic back to home arrow link

--- frontend/components/Menu.js
+++ frontend/components/Menu.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Link from "next/link";
 
-const Fragment = React.Fragment;
 
 class Menu extends Component {
 	constructor() {
@@ -14,12 +13,26 @@ class Menu extends Component {
 	}
 
 	render() {
+		let backToHome;
+		if (this.props.backToHome === undefined)
+			backToHome = true;
+		else
+			backToHome = this.props.backToHome
+
 		return (
 			<header className="site-header">
 				<nav className="container">
+					<div className="site-header-wrapper">
+					{ backToHome &&
+
+						<Link href="/">
+							<svg className="back-arrow" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1280 800v192q0 13-9.5 22.5t-22.5 9.5h-352v192q0 14-9 23t-23 9q-12 0-24-10l-319-319q-9-9-9-23t9-23l320-320q9-9 23-9 13 0 22.5 9.5t9.5 22.5v192h352q13 0 22.5 9.5t9.5 22.5zm160 96q0-148-73-273t-198-198-273-73-273 73-198 198-73 273 73 273 198 198 273 73 273-73 198-198 73-273zm224 0q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z"/></svg>
+						</Link>
+					}
 					<Link href="/">
-						<a>Home</a>
+						<div className="logo">BINARY SUNSET</div>
 					</Link>
+					</div>
 				</nav>
 			</header>
 		)
