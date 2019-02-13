410a6a8522546c0b16d2910b095f0d2105a69f55
Mike Reinhard <mike.reinhard@fractureme.com>
2019-02-13 12:21:45 -0500

    use full viewport on iphone X styles

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -16,7 +16,7 @@ class Header extends Component {
                     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
                     <meta
                         name="viewport"
-                        content="width=device-width, initial-scale=1"
+                        content="width=device-width, initial-scale=1, viewport-fit=cover"
                     />
                     <meta charSet="utf-8" />
                     <title>Binary Sunset</title>
